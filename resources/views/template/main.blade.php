d87df10d2f2f0376c29fdf824a9a8ad454bbfed2
Okyop <pcombes80@gmail.com>
2021-02-05 10:12:28 +0100

    third commit

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -9,8 +9,8 @@
         <meta content="" name="keywords">
       
         <!-- Favicons -->
-        <link href="assets/img/favicon.png" rel="icon">
-        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+        <link href="{{asset('/img/favicon.png')}}" rel="icon">
+        <link href="{{asset('/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
       
         <!-- Bootstrap core CSS -->
         <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
@@ -19,15 +19,15 @@
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
       
         <!-- Vendor CSS Files -->
-        <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-        <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-        <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-        <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
-        <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+        <link href="{{asset('vendor/icofont/icofont.min.css')}}" rel="stylesheet">
+        <link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+        <link href="{{asset('vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+        <link href="{{asset('vendor/venobox/venobox.css')}}" rel="stylesheet">
+        <link href="{{asset('vendor/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
       
         <!-- Template Main CSS File -->
-        <link href="assets/css/style.css" rel="stylesheet">
+        <link href="{{asset('css/style.css')}}" rel="stylesheet">
       
         <!-- =======================================================
         * Template Name: Resi - v2.2.1
@@ -41,7 +41,19 @@
     @yield('content')
     @include('partials.footer')
 
+    <!-- Vendor JS Files -->
+  <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('vendor/jquery.easing/jquery.easing.min.js')}}"></script>
+  <script src="{{asset('vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{asset('vendor/waypoints/jquery.waypoints.min.js')}}"></script>
+  <script src="{{asset('vendor/counterup/counterup.min.js')}}"></script>
+  <script src="{{asset('vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{asset('vendor/venobox/venobox.min.js')}}"></script>
+  <script src="{{asset('vendor/owl.carousel/owl.carousel.min.js')}}"></script>
 
+  <!-- Template Main JS File -->
+    <script src="{{('asset/js/main.js')}}"></script>
     <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 </body>
